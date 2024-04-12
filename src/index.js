5e129fabdceeea9d0f0da69e6e3167efba7c0f9e
Maksim Goncharovskiy <maksimgoncharovskiy@gmail.com>
2024-04-12 21:57:36 +0300

    Изменил index.jsx и GlobalStyle. Сделал градиентный фон.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
+import { GlobalStyle } from './GlobalStyle.tsx';
+
+// Типографика, имеющая размеры, зависимые от типа устройства
+import { DeviceThemeProvider } from '@salutejs/plasma-ui/components/Device';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <DeviceThemeProvider>
+      <GlobalStyle />
+      <App />
+  </DeviceThemeProvider>,
+  document.getElementById('root'),
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
