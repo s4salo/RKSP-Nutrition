0394fe3ddf37f72abce286a7e0d16b579eb55863
s4salo <gav-ya_030504@mail.ru>
2024-05-31 22:35:08 +0300

    stili

--- src/include/MakeTable.jsx
+++ src/include/MakeTable.jsx
@@ -6,8 +6,8 @@ export function MakeTable(args){
     return(
         <table class="m-auto">
             <tr>
-                <th style={{borderTopLeftRadius: "50px"}}>Белки</th>
-                <td style={{borderTopRightRadius: "50px"}}>{args.protein[0]} {args.protein[1]}</td>
+                <th style={{borderTopLeftRadius: "20px"}}>Белки</th>
+                <td style={{borderTopRightRadius: "20px"}}>{args.protein[0]} {args.protein[1]}</td>
             </tr>
             <tr>
                 <th>Жиры</th>
@@ -18,8 +18,8 @@ export function MakeTable(args){
                 <td>{args.carbohydrates[0]} {args.carbohydrates[1]}</td>
             </tr>
             <tr>
-                <th style={{borderBottomLeftRadius: "50px"}}>Калории</th>
-                <td style={{borderBottomRightRadius: "50px"}}>{args.calories[0]} {args.calories[1]}</td>
+                <th style={{borderBottomLeftRadius: "20px"}}>Калории</th>
+                <td style={{borderBottomRightRadius: "20px"}}>{args.calories[0]} {args.calories[1]}</td>
             </tr>
         </table>
     )
